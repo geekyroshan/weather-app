141226bf66bef906d543f9d6d883ac4b78389dad
Roshan Kharel <kharelroshan128@gmail.com>
2025-01-13 18:30:00 +0000

    Add examples to documentation

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+// Updated: 2025-04-25T11:49:03.497Z
